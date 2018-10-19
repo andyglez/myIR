e317cf66dee5ed37ca319b8d8b94b92356b06044
Andy Gonzalez <aglez9538@gmail.com>
2018-10-19 17:47:54 -0400

    Routing the about page

--- ui/app.py
+++ ui/app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+#@app.before_request
+#def before_request():
+    # When you import jinja2 macros, they get cached which is annoying for local
+    # development, so wipe the cache every request.
+#    if 'localhost' in request.host_url or '0.0.0.0' in request.host_url:
+#        app.jinja_env.cache = {}
 
 @app.route('/')
 def layout():
     return render_template('layout.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
