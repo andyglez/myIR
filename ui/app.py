1bd7f51aac81ed3b3c8a8d513eb254b5d33f3fa8
Andy Gonzalez <aglez9538@gmail.com>
2018-10-20 23:51:01 -0400

    Adding an index subview

--- ui/app.py
+++ ui/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, session
+from flask import Flask, render_template, url_for, request, session, flash, redirect
 import os
 
 app = Flask(__name__)
@@ -9,14 +9,31 @@ if __name__ == '__main__':
 
 @app.route('/', methods=['GET','POST'])
 def layout():
-    session['username'] = 'andy'
+    if 'user' in session:
+        return redirect(url_for('index'))
+    session['user'] = 'andy'    
+    return render_template('index.html')
+
+@app.route('/index', methods=['GET','POST'])
+def index():
     if request.method == 'POST':
         if 'path' in session:
-            if not request.form['path'] == session['path']:
-                return render_template('about.html')
+            if request.form['path'] == session['path']:
+                if 'build' in session and session['build'] == True:
+                    flash('Model is already constructed', 'warning')
+                else:
+                    session['build'] = False
+            else:
+                session['build'] = True
+                session['path'] = request.form['path']
+                if os.path.exists(session['path']):
+                    flash('Ok it does exist', 'message')
+                else:
+                    flash('Path is wrong', 'error')
         else:
             session['path'] = request.form['path']
-    return render_template('layout.html')
+        return redirect(url_for('index'))
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
