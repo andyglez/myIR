7d76b081bff26f3c0c810de4854d2f613b5536a2
Andy Gonzalez <aglez9538@gmail.com>
2018-10-20 21:13:18 -0400

    Adding functionality to Model building views

--- ui/app.py
+++ ui/app.py
@@ -1,16 +1,21 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, session
+import os
 
 app = Flask(__name__)
+app.secret_key = str(os.urandom(24))
+if __name__ == '__main__': 
+    app.run(debug=True, host='0.0.0.0')
 
-#@app.before_request
-#def before_request():
-    # When you import jinja2 macros, they get cached which is annoying for local
-    # development, so wipe the cache every request.
-#    if 'localhost' in request.host_url or '0.0.0.0' in request.host_url:
-#        app.jinja_env.cache = {}
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def layout():
+    session['username'] = 'andy'
+    if request.method == 'POST':
+        if 'path' in session:
+            if not request.form['path'] == session['path']:
+                return render_template('about.html')
+        else:
+            session['path'] = request.form['path']
     return render_template('layout.html')
 
 @app.route('/about')
