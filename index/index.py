8ab2ffdf6fda59f925272f1c700ce54ee9aed0ed
Andy Gonzalez <aglez9538@gmail.com>
2018-11-02 11:55:17 -0400

    Defining backpropagate algorithm

--- index/index.py
+++ index/index.py
@@ -2,12 +2,13 @@ import io
 import json
 import os
 from time import time
+from numpy import dot
 
 
-def process(data):
+def process(data, index):
     result = {'success' : True}
     if data['action'] == 'create':
-        globals()['index'] = create_index(data['data'])
+        create_index(data, index)
     elif data['action'] == 'add' or data['action'] == 'update':
         globals()['index'][data['key']] = data['value']
     elif data['action'] == 'delete':
@@ -58,12 +59,50 @@ def sort(value):
     #            aux[j] = temp
     return aux
 
-def create_index(data):
-    result = {}
-    for node in data:
-        result[node['key']] = node
-    printjson(result, 'index.json')
-    return result
+
+def create_index(data, index):
+    neural_network = data['data']
+    index['data'] = train(neural_network, data['tf'], data['idf'])
+    printjson(index, index['path'])
+
+
+def train(neural_network, tf, idf):
+    inputs = [[1 for j in range(len(tf)) if j == i and 0 if not j == i] for i in range(len(tf))]
+    targets = [[1 for tf in tf_i if idf * tf > 0 and 0 if idf * tf == 0] for idf, tf_i in zip(idf, tf)]
+
+    for i in range(10):
+        for input_vector, target_vector in zip(inputs, targets):
+            backpropagate(neural_network, input_vector, target_vector)
+    return neural_network
+
+
+def backpropagate(neural_network, input_vector, targets):
+    outputs = feed_forward(neural_network, input_vector)
+    outputs.insert(0, input_vector)
+    count = 1
+    while len(outputs) > 1:
+        output_deltas = [output * (1 - output) * (output - target)
+                         for output, target in zip(outputs[-1], targets)]
+
+        for i, output_neuron in enumerate(neural_network[-count]):
+            for j, prev_output in enumerate(outputs[-1]):
+                output_neuron[j] -= output_deltas[i] * prev_output
+
+        prev_deltas = [prev_output * (1 - prev_output) *
+                       dot(output_deltas, [n[i] for n in neural_network[-count]])
+                       for i, prev_output in enumerate(neural_network[-(count+1)])]
+
+        outputs.remove(outputs[-1])
+        count += 1
+        targets = []
+
+        for i, prev_neuron in enumerate(outputs[-1]):
+            targets.append(prev_neuron - prev_deltas * outputs[-2])
+
+
+def feed_forward(neural_network, input_vector):
+    return 0
+
 
 def printjson(data, output):
     data['time'] = time()
@@ -74,19 +113,17 @@ def printjson(data, output):
         outfile.write(text)
 
 if __name__ == '__main__':
-    t = time()
+    index = {}
     try:
-        globals()['index'] = {}
-        with io.open(os.path.curdir + '/index/index.json', 'r', encoding='utf8') as index:
-            globals()['index'] = json.load(index)
+        with io.open(os.path.curdir + '/index/index.json', 'r', encoding='utf8') as model:
+            index = json.load(model)
+            index['current'] = os.path.curdir + '/index/index.json'
     except:
         pass
     try:
         data = {}
         with io.open(os.path.curdir + '/json/in.index.json', 'r', encoding='utf8') as data_file:
             data = json.load(data_file)
-            process(data)
-            t = time()
-    except Exception as e:
-        printjson({'success': False, 'message': str(e)}, os.path.curdir + '/json/out.index.json')
+        process(data, index)
+    except:
         pass
